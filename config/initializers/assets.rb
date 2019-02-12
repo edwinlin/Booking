e00a51e87f430202be15aa2d5bd889f170081c4d
github username <github email address>
2019-02-12 12:38:37 -0500

    made BLOCKED dates red

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( listings.css )
+Rails.application.config.assets.precompile += %w( bookings.css )
+Rails.application.config.assets.precompile += %w( users.css )
